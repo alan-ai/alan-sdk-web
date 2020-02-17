4b3dfe273d11458b65f6cecc353497a49f467654
Anna Miroshnichenko <anna@alan.app>
2020-02-17 15:04:34 +0500

    Add example of using the Alan Button with an Ember app

---
+++ examples/EmberAppWithAlan/app/app.js
@@ -0,0 +1,19 @@
+import Application from '@ember/application';
+import Resolver from './resolver';
+import loadInitializers from 'ember-load-initializers';
+import config from './config/environment';
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const App = Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver
+});
+
+loadInitializers(App, config.modulePrefix);
+
+alanBtn({ 
+  key: 'e3018cc76639126f974f1bf6b6929c1b2e956eca572e1d8b807a3e2338fdd0dc/stage'
+ });
+
+export default App;
