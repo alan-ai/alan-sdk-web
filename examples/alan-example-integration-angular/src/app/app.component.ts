ed7adec328b7e1bf9d06a6830adb55f2a140ef79
aermilin <etioff@gmail.com>
2020-06-22 23:58:26 +0300

    angular - update prroject key and doc link

--- examples/alan-example-integration-angular/src/app/app.component.ts
+++ examples/alan-example-integration-angular/src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
 
   constructor(){
     this.alanBtnInstance = alanBtn({ 
-      key: 'e3018cc76639126f974f1bf6b6929c1b2e956eca572e1d8b807a3e2338fdd0dc/stage'
+      key: '314203787ccd9370974f1bf6b6929c1b2e956eca572e1d8b807a3e2338fdd0dc/prod'
      });
   }
 }
