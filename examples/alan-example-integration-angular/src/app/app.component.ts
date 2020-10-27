ce3b2506f4e0cc1b5b379fee400defd7f9db6a54
Anna Miroshnichenko <anna@alan.app>
2020-10-28 00:31:14 +0500

    Update example of using the AlanButton with Angular

--- examples/alan-example-integration-angular/src/app/app.component.ts
+++ examples/alan-example-integration-angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import alanBtn from "@alan-ai/alan-sdk-web";
+import { AlanButton } from '@alan-ai/alan-sdk-web/dist/AlanButton';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,31 @@ import alanBtn from "@alan-ai/alan-sdk-web";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  alanBtnInstance;
+  alanBtnInstance: AlanButton;
+  constructor() {
+    // Example of the adding the Alan Button to the page
+    this.alanBtnInstance = alanBtn({
+      key: '314203787ccd9370974f1bf6b6929c1b2e956eca572e1d8b807a3e2338fdd0dc/prod',
+      onCommand: (commandData: { command: string }) => {
+        console.log(commandData);
+        if (commandData.command === 'command-example') {
+          document.getElementById('rocket').style.transform = 'rotate(270deg)';
+        }
+      }
+    });
+  }
 
-  constructor(){
-    this.alanBtnInstance = alanBtn({ 
-      key: '314203787ccd9370974f1bf6b6929c1b2e956eca572e1d8b807a3e2338fdd0dc/prod'
-     });
+  public activate() {
+    this.alanBtnInstance.activate();
+  }
+
+  public activateAndPlayText() {
+    this.alanBtnInstance.activate().then(() => {
+      this.alanBtnInstance.playText('Hi');
+    });
+  }
+
+  public deactivate() {
+    this.alanBtnInstance.deactivate();
   }
 }
