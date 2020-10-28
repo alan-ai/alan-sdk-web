e4fd260f9ac76594329b7f2734617e3beb22fa78
Anna Miroshnichenko <anna@alan.app>
2020-10-29 00:45:27 +0500

    Update example of the using the Alan Button in React app

--- examples/alan-example-integration-react/src/App.js
+++ examples/alan-example-integration-react/src/App.js
@@ -1,32 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
 import alanBtn from "@alan-ai/alan-sdk-web";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+let alanBtnInstance;
 
-    this.alanBtnInstance = null;
-  }
+function App() {
+  const alanBtnContainer = useRef();
 
-  componentDidMount() {
-    this.alanBtnInstance = alanBtn({ 
-      key: '314203787ccd9370974f1bf6b6929c1b2e956eca572e1d8b807a3e2338fdd0dc/prod'
-     });
-  }
+  useEffect(() => {
+    alanBtnInstance = alanBtn({
+      key: '314203787ccd9370974f1bf6b6929c1b2e956eca572e1d8b807a3e2338fdd0dc/prod',
+      rootEl: alanBtnContainer.current
+    });
+  }, []);
 
-  render() {
-    return <div className="App">
-      <header className="App-header">
-        <img src="https://alan.app/voice/images/previews/preview.jpg"
-          className="Alan-logo" alt="logo" />
-        <ul>
-          <li>Say: "Hi!"</li>
-          <li>Say: "What is Alan Platform?"</li>
-        </ul>
-      </header>
-    </div>
-  }
+  return <div className="App">
+    <header className="App-header">
+      <img src="https://alan.app/voice/images/previews/preview.jpg"
+        className="Alan-logo" alt="logo" />
+      <ul>
+        <li>Say: "Hi!"</li>
+        <li>Say: "What is Alan Platform?"</li>
+      </ul>
+    </header>
+    <div ref={alanBtnContainer}></div>
+  </div>;
 }
 
 export default App;
