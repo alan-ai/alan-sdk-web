64027281ef21aa62e47572361a8fd410ccbc0e77
Anna Miroshnichenko <anna@alan.app>
2020-10-29 01:09:12 +0500

    Fix React warning in the alan-example-integration-react app

--- examples/alan-example-integration-react/src/App.js
+++ examples/alan-example-integration-react/src/App.js
@@ -2,13 +2,11 @@ import React, { useEffect, useRef } from 'react';
 import './App.css';
 import alanBtn from "@alan-ai/alan-sdk-web";
 
-let alanBtnInstance;
-
 function App() {
   const alanBtnContainer = useRef();
 
   useEffect(() => {
-    alanBtnInstance = alanBtn({
+    alanBtn({
       key: '314203787ccd9370974f1bf6b6929c1b2e956eca572e1d8b807a3e2338fdd0dc/prod',
       rootEl: alanBtnContainer.current
     });
