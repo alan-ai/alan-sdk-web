d0b5f3477bcbc13b70672298fcc9703d6a3d1c37
lailys <laily@alan.app>
2022-05-05 13:06:28 -0700

    Add appointment app to examples

---
+++ examples/alan-example-integeration-react-appointment/appointment-frontend/build/precache-manifest.a7f0f45b48bcb4609f69a452ddf2bae7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a9def937114e3de9ee3",
+    "url": "/static/css/main.b588e0e7.chunk.css"
+  },
+  {
+    "revision": "9a9def937114e3de9ee3",
+    "url": "/static/js/main.9a9def93.chunk.js"
+  },
+  {
+    "revision": "be040bf3f7f14408a887",
+    "url": "/static/js/1.be040bf3.chunk.js"
+  },
+  {
+    "revision": "f8b088cab2f52fc6c8d7",
+    "url": "/static/js/2.f8b088ca.chunk.js"
+  },
+  {
+    "revision": "405eeea52d0d05c68bdf",
+    "url": "/static/js/runtime~main.405eeea5.js"
+  },
+  {
+    "revision": "59be907bd04b913c10711b9b096f3822",
+    "url": "/static/media/background-img-home.59be907b.jpg"
+  },
+  {
+    "revision": "97fc6fcc609d5ce40d423409d85dcdb7",
+    "url": "/index.html"
+  }
+];
