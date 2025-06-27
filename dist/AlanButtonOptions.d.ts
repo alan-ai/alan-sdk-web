c7e91edc6a68161a63fd33a9f14c6c9614c0a4c8
Anna Miroshnichenko <miroshnichenkoanna74@gmail.com>
2025-06-27 21:13:28 +0500

    Support adding info element for the Text Chat chat in the footer Fix some ui issues for message rendering in the Text Chat

--- dist/AlanButtonOptions.d.ts
+++ dist/AlanButtonOptions.d.ts
@@ -25,5 +25,6 @@ export interface AlanButtonOptions {
         onMinimize?: () => void;
         onOpen?: () => void;
         headerElement?: HTMLDivElement;
+        footerInfoElement?: HTMLDivElement;
     }
 }
